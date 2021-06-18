c766a6524d4a4e8d57ff22917eb45356dfd2a987
mini <jongmin4422@naver.com>
2021-06-19 00:18:27 +0900

    change code

--- plot.js
+++ plot.js
@@ -22,6 +22,7 @@ function read(data){
     function readChart(index=30) {
         readSy=[], readHy=[], readSS=[], readHH=[], readSpre=[], readHpre=[], readdate=[];
         var k=0, d=0, s=0, sp=0, hp=0, ss=0, hh=0, p=0, p2=0;
+        hcorrect=0, scorrect=0;
         // console.log(SS.slice(-index-1,-1));
         for (let i = 0; i < index; i++) {
             k = k + Number(Sy[Sy.length-index+i]);
@@ -36,9 +37,15 @@ function read(data){
             readHH.push(hh);
             readSpre.push(sp);
             readHpre.push(hp);
+
+            //적중률 계산
+            hcorrect = hcorrect+hh;
+            scorrect = scorrect+ss;
+            console.log(scorrect);
         }
         readdate = date.slice(-index-1,-1);
-        return readHy, readHpre, readHH, readdate, readSy, readSpre, readSS;
+        console.log(hcorrect);
+        return readHy, readHpre, readHH, readdate, readSy, readSpre, readSS, hcorrect, scorrect;
     }
     
     function makeChart() {
@@ -192,13 +199,15 @@ function read(data){
         document.getElementById('btn_month').onclick = function(){
             myChart.destroy();
             myChart2.destroy();
-            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS = readChart(30);
+            var index = 30;
+            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS, hcorrect, scorrect= readChart(index);
+            console.log(hcorrect);
             var data = myChart.config.data;        
             data.datasets[0].data = readHy;
             data.datasets[1].data = readHpre;
             data.datasets[2].data = readHH;
             data.labels = readdate;
-            myChart.config.options.scales.y.max = 30;
+            myChart.config.options.scales.y.max = index;
             myChart = new Chart(ctx, config);
             myChart.update();
             var data2 = myChart2.config.data;
@@ -206,7 +215,7 @@ function read(data){
             data2.datasets[1].data = readSpre;
             data2.datasets[2].data = readSS;
             data2.labels = readdate;
-            myChart2.config.options.scales.y.max = 30;
+            myChart2.config.options.scales.y.max = index;
             myChart2 = new Chart(sctx, sconfig);
             myChart2.update();
 
@@ -214,23 +223,28 @@ function read(data){
             jQuery('.sredhigh > span > i').text(" " + smonth_up + "%");
             jQuery('.bluehigh > span > i').text(" " + month_down + "%");
             jQuery('.sbluehigh > span > i').text(" " +smonth_down + "%");
+            jQuery('.hb-highlight').text(" " +(hcorrect/index*100).toFixed(2) + "%");
+            jQuery('.sb-highlight').text(" " +(scorrect/index*100).toFixed(2) + "%");
+            jQuery('.h-explain, s-explain').text("30일, 개장일 기준");         
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:0.4},200);
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:1},200);
             jQuery('#btn_month').css('background', 'linear-gradient(to bottom, #0093DD, #5AC3E1');
             jQuery('#btn_year, #btn_6month, #btn_ori').css('background', 'white');
+
         }
 
         
         document.getElementById('btn_6month').onclick = function(){
             myChart.destroy();
             myChart2.destroy();
-            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS = readChart(120);
+            var index = 120;
+            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS, hcorrect, scorrect = readChart(index);
             var data = myChart.config.data;        
             data.datasets[0].data = readHy;
             data.datasets[1].data = readHpre;
             data.datasets[2].data = readHH;
             data.labels = readdate;
-            myChart.config.options.scales.y.max = 120;            
+            myChart.config.options.scales.y.max = index;            
             myChart = new Chart(ctx, config);        
             myChart.update();
             var data2 = myChart2.config.data;
@@ -238,7 +252,7 @@ function read(data){
             data2.datasets[1].data = readSpre;
             data2.datasets[2].data = readSS;
             data2.labels = readdate;
-            myChart2.config.options.scales.y.max = 120;
+            myChart2.config.options.scales.y.max = index;
             myChart2 = new Chart(sctx, sconfig);
             myChart2.update();
 
@@ -247,6 +261,9 @@ function read(data){
             jQuery('.sredhigh > span > i').text(" " + smonth6_up + "%");
             jQuery('.bluehigh > span > i').text(" " + month6_down + "%");
             jQuery('.sbluehigh > span > i').text(" " +smonth6_down + "%");
+            jQuery('.hb-highlight').text(" " +(hcorrect/index*100).toFixed(2) + "%");
+            jQuery('.sb-highlight').text(" " +(scorrect/index*100).toFixed(2) + "%");
+            jQuery('.h-explain, s-explain').text("120일, 개장일 기준");
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:0.4},200);
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:1},200);
             jQuery('#btn_6month').css('background', 'linear-gradient(to bottom, #0093DD, #5AC3E1');
@@ -257,13 +274,14 @@ function read(data){
         document.getElementById('btn_year').onclick = function(){
             myChart.destroy();
             myChart2.destroy();
-            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS = readChart(250);
+            index = 250;
+            readHy, readHpre, readHH, readdate, readSy, readSpre, readSS, hcorrect, scorrect = readChart(index);
             var data = myChart.config.data;        
             data.datasets[0].data = readHy;
             data.datasets[1].data = readHpre;
             data.datasets[2].data = readHH;
             data.labels = readdate;
-            myChart.config.options.scales.y.max = 250;
+            myChart.config.options.scales.y.max = index;
             myChart = new Chart(ctx, config);
             myChart.update();
             var data2 = myChart2.config.data;
@@ -271,14 +289,18 @@ function read(data){
             data2.datasets[1].data = readSpre;
             data2.datasets[2].data = readSS;
             data2.labels = readdate;
-            myChart2.config.options.scales.y.max = 250;
+            myChart2.config.options.scales.y.max = index;
             myChart2 = new Chart(sctx, sconfig);
             myChart2.update();
 
             jQuery('.redhigh > span > i').text(" " + year_up + "%");
             jQuery('.sredhigh > span > i').text(" " + syear_up + "%");
             jQuery('.bluehigh > span > i').text(" " + year_down + "%");
-            jQuery('.sbluehigh > span > i').text(" " +syear_down + "%");            
+            jQuery('.sbluehigh > span > i').text(" " +syear_down + "%");
+            jQuery('.hb-highlight').text(" " +(hcorrect/index*100).toFixed(2) + "%");
+            jQuery('.sb-highlight').text(" " +(scorrect/index*100).toFixed(2) + "%");
+            jQuery('.h-explain, s-explain').text("250일, 개장일 기준");         
+            // jQuery('.s-explain').text("1개월, 개장일 기준");
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:0.4},200);
             jQuery(".redhigh > span > i, .sredhigh > span > i, .bluehigh > span > i, .sbluehigh > span > i").animate({opacity:1},200);            
             jQuery('#btn_year').css('background', 'linear-gradient(to bottom, #0093DD, #5AC3E1');
@@ -288,12 +310,5 @@ function read(data){
         
     }
     makeChart();
-}
-function addData(chart, label, data) {
-    chart.data.labels.push(label);
-    chart.data.datasets.forEach((dataset) => {
-        dataset.data.push(data);
-    });
-    chart.update();
 }
 // 위 배열들은 모두 csv파일에서 필요한 부분 전체를 읽어온 뒤 각 기간 인덱스 버튼에 맞게 설정
